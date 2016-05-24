7a8637cb0fa06acbbfa4e7341042fd9dc13d08c5
elcid73 <lanz91@hotmail.com>
2016-05-24 13:15:13 -0700

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,10 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+baltimore<-subset(NEI,fips=='24510')
+
+png(filename='plot2.png')
+
+barplot(tapply(X=baltimore$Emissions, INDEX=baltimore$year, FUN=sum), main='Total Emission in Baltimore, MD',xlab='Year', ylab=expression('PM'[2.5]))
+
+dev.off()
