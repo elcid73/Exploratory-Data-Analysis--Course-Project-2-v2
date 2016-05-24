1a9c83ce4c912a3c0527f06facd2abb444faf36f
elcid73 <lanz91@hotmail.com>
2016-05-24 14:25:05 -0700

    Add files via upload

---
+++ plot5.R
@@ -0,0 +1,16 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+library(ggplot2)
+
+baltimore<-subset(NEI,fips=='24510'& type == 'ON-ROAD')
+
+
+baltimore_motor<-aggregate(Emissions ~ year,baltimore,sum)
+
+
+png('plot5.png')
+ggplot(baltimore_motor, aes(factor(year), Emissions)) + geom_bar(stat="identity") + xlab("year") + ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from motor vehicle in Baltimore, Maryland from 1999 to 2008')
+
+dev.off()
